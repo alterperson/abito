c57a8fabba9a04fbadbbb57dd1077264c99d1b55
alterperson <vad.mk@ya.ru>
2021-07-29 12:02:28 +0300

    Адаптив для слайдера

--- js/main.js
+++ js/main.js
@@ -12,10 +12,28 @@ menuCloseBtn.addEventListener('click', () => {
 
 const swiper = new Swiper(".mySwiper", {
   spaceBetween: 10,
-  slidesPerView: 7,
+  slidesPerView: 4,
   freeMode: true,
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 4,
+      spaceBetween: 10
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    },
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 10
+    },
+    1200: {
+      slidesPerView: 7,
+      spaceBetween: 10
+    },
+  },
 });
 const swiper2 = new Swiper(".mySwiper2", {
   spaceBetween: 10,
